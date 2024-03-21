01352090b97a02c3e2c43749944833debc50d53a
VicS18 <vitor.mtsantos@ua.pt>
2024-03-21 18:55:46 +0000

    [HW1] Project setup

---
+++ HW1/hw1/src/main/java/tqs/a107186/App.java
@@ -0,0 +1,13 @@
+package tqs.a107186;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
